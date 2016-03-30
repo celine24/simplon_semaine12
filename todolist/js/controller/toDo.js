20fe8c199a6297e4b9c4b732d03f8daaeafbdf9d
celine24 <peyrot.celine@gmail.com>
2016-03-30 14:40:29 +0000

    to do list fonctionnel

--- todolist/js/controller/toDo.js
+++ todolist/js/controller/toDo.js
@@ -1,6 +1,15 @@
-app.controller('toDoCtrl', ['$scope', '$http', 'filterFilter', function ($scope, $http, filterFilter) {
+app.directive('ngBlur', function() {
+    return function(scope, elem, attrs) {
+        elem.bind("blur", function(){
+            scope.$apply(attrs.ngBlur);
+        })
+    }
+});
+
+app.controller('toDoCtrl', ['$scope', '$http', 'filterFilter', '$location', function ($scope, $http, filterFilter, $location) {
     $scope.tasks = [];
     $scope.todoName = "Chargement...";
+    $scope.statusFilter = {};
     
     $http.get('tasks.php').success(function(data){
         $scope.tasks = data;
@@ -30,10 +39,29 @@ app.controller('toDoCtrl', ['$scope', '$http', 'filterFilter', function ($scope,
         $scope.tasksLength = filterFilter($scope.tasks, {checked:false}).length;
         $scope.allchecked = !$scope.tasksLength;
     }, true);
+
+
+    if ($location.path() == '') { $location.path("/"); }
+    $scope.location = $location;
+    $scope.$watch('location.path()', function(path) {
+        $scope.statusFilter =
+            (path == "/active") ? {checked : false} :
+            (path == "/done") ? {checked : true} :
+            (path == "/") ? { } :
+            null;
+    });
     
     $scope.checkAll = function (allchecked) {
         $scope.tasks.forEach(function(task) {
             task.checked = allchecked;
         });
+    };
+
+    $scope.editTask = function (task) {
+        task.editing = false;
+    };
+
+    $scope.removeAll = function () {
+        $scope.tasks = [];
     };
 }]);
